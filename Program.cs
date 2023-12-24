ec6137c201e083f6011c6b8a1a142053e6333cbd
Sagar <sagarbndkr@gmail.com>
2023-12-24 10:42:02 +0530

    1375. Number of Times Binary String Is Prefix-Aligned

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static int Solve(int[] nums)
+    {
+        int maxIndex=0;
+        int count=0;
+
+        for(int i=0;i<nums.Length;i++)
+        {
+            maxIndex=Math.Max(maxIndex,nums[i]);
+
+            if(maxIndex==i+1) count++;
+        }
+
+        return count;
+    }
+    public static void Main(string[] args)
+    {
+        int[] flips = {3,2,4,1,5};
+        Solve(flips);
+    }
+}
