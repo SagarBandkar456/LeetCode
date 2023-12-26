ad5b9c49614ebe6ade04e90a4ffd1f748daa9e0d
Sagar <sagarbndkr@gmail.com>
2023-12-26 09:38:26 +0530

    1535. Find the Winner of an Array Game

--- Program.cs
+++ Program.cs
@@ -1,70 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
-    public static int MinDistance(int[] dist, bool[] sptSet, int v)
+    public static int GetWinner(int[] arr, int k)
     {
-        int min=Int32.MaxValue, minIndex=-1;
+        int res = arr[0];
+        int cnt = 0;
 
-        for(int i=0;i<v;i++)
+        for (int i = 1; i < arr.Length; i++)
         {
-            if(sptSet[i]==false && dist[i]<min)
+            if (arr[i] > res)
             {
-                min=dist[i];
-                minIndex=i;
+                res = arr[i];
+                cnt = 0;
             }
-        }
-        
-        return minIndex;
-    }
+            cnt++;
 
-    public static int CountPaths(int n, int[][] roads) {
-        int src=0;
-        int dest=n-1;
-
-        int[] dist=new int[n];
-        bool[] sptSet=new bool[n];
-
-        for(int i=0;i<n;i++)
-        {
-            sptSet[i]=false;
-            dist[i]=Int32.MaxValue;
+            if (cnt == k) break;
         }
 
-        dist[src]=0;
-
-        for(int i=0;i<n;i++)
-        {
-            int u=MinDistance(dist,sptSet,n);
-
-            sptSet[u]=true;
-            for (int v = 0; v < n; v++)
- 
-                if (!sptSet[v] && roads[u][v] != 0
-                    && dist[u] != Int32.MaxValue
-                    && dist[u] + roads[u][v] < dist[v])
-                    dist[v] = dist[u] + roads[u][v];
-        }
-
-        return dist[dest];
+        return res;
     }
     public static void Main(string[] args)
     {
-        int n = 7;
-        int[][] roads = new int[][]
-        {
-            new int[]{0,6,7},
-            new int[]{0,1,2},
-            new int[]{1,2,3},
-            new int[]{1,3,3},
-            new int[]{6,3,3},
-            new int[]{3,5,1},
-            new int[]{6,5,1},
-            new int[]{2,5,1},
-            new int[]{0,4,5},
-            new int[]{4,6,2},
-        };
-
-        CountPaths(n,roads);
+        int[] arr = { 2, 1, 3, 5, 4, 6, 7 };
+        int k = 2;
+        Console.WriteLine(GetWinner(arr, k));
     }
 }
--- obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6137c201e083f6011c6b8a1a142053e6333cbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda04e3e5b68160c291d4d686f639dc0f16a07a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
