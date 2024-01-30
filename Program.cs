4806d0406b2212cc9573125f199a01a7d35f2183
Sagar Bandkar <sagarbndkr@gmail.com>
2024-01-30 10:30:18 +0530

    62. Unique Paths

--- Program.cs
+++ Program.cs
@@ -5,30 +5,24 @@ using System.Text;
 
 class Program
 {
-    public static int[] CountBits(int n)
+    static int[,] dp = new int[101, 101];
+    public static int Solve(int x, int y, int m, int n)
     {
-        int[] res = new int[n + 1];
-        for (int i = 0; i <= n; i++)
-        {
-            res[i] = CountSetBits(i);
-        }
-        return res;
-    }
+        if (x >= m || y >= n) return 0;
+        if (x == m - 1 && y == n - 1) return 1;
 
-    static int CountSetBits(int n)
-    {
-        int count = 0;
-        while (n > 0)
-        {
-            n = n & (n - 1);
-            count++;
-        }
-
-        return count;
+        if (dp[x, y] != -1) return dp[x, y];
+        return dp[x, y] = Solve(x + 1, y, m, n) + Solve(x, y + 1, m, n);
     }
     public static void Main(string[] args)
     {
-        int n = 2;
-        Console.WriteLine(CountBits(n));
+        for (int i = 0; i < 101; i++)
+        {
+            for (int j = 0; j < 101; j++)
+            {
+                dp[i, j] = -1;
+            }
+        }
+        Console.WriteLine(Solve(0, 0, 3, 7));
     }
 }
