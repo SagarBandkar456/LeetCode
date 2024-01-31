07113ece8a9444b6689285cbada0129e3af70228
Sagar Bandkar <sagarbndkr@gmail.com>
2024-01-31 20:12:06 +0530

    72. Edit Distance

--- Program.cs
+++ Program.cs
@@ -5,24 +5,49 @@ using System.Text;
 
 class Program
 {
-    static int[,] dp = new int[101, 101];
-    public static int Solve(int x, int y, int m, int n)
+    static int[,] dp = new int[501, 501];
+    static int Solve(int m, int n, string word1, string word2)
     {
-        if (x >= m || y >= n) return 0;
-        if (x == m - 1 && y == n - 1) return 1;
+        if (m == word1.Length) return word2.Length - n;
 
-        if (dp[x, y] != -1) return dp[x, y];
-        return dp[x, y] = Solve(x + 1, y, m, n) + Solve(x, y + 1, m, n);
+        if (n == word2.Length) return word1.Length - m;
+
+        if (dp[m, n] != -1) return dp[m, n];
+
+        int res = 0;
+        if (word1[m] == word2[n])
+        {
+            return dp[m, n] = Solve(m + 1, n + 1, word1, word2);
+        }
+        else
+        {
+            //Insert
+            int insert = 1 + Solve(m, n + 1, word1, word2);
+            //Delete
+            int delete = 1 + Solve(m + 1, n, word1, word2);
+            //Replace
+            int replace = 1 + Solve(m + 1, n + 1, word1, word2);
+
+            res = Math.Min(insert, Math.Min(delete, replace));
+        }
+
+        return dp[m, n] = res;
+    }
+    public static int MinDistance(string word1, string word2)
+    {
+        return Solve(0, 0, word1, word2);
     }
     public static void Main(string[] args)
     {
-        for (int i = 0; i < 101; i++)
+        string word1 = "horse", word2 = "ros";
+
+        for (int i = 0; i < 501; i++)
         {
-            for (int j = 0; j < 101; j++)
+            for (int j = 0; j < 501; j++)
             {
                 dp[i, j] = -1;
             }
         }
-        Console.WriteLine(Solve(0, 0, 3, 7));
+        Console.WriteLine(MinDistance(word1, word2));
     }
 }
