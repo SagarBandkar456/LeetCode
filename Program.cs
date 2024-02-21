f2153e0ef17014b5ff1ce7c5276a803d9d24667f
Sagar Bandkar <sagarbndkr@gmail.com>
2024-02-21 11:02:29 +0530

    2656. Maximum Sum With Exactly K Elements

--- Program.cs
+++ Program.cs
@@ -5,36 +5,25 @@ using System.Text;
 
 class Program
 {
-    static int[] dp = new int[401];
-    static int Solve(int[] nums, int n)
+    public static int MaximizeSum(int[] nums, int k)
     {
-        if (n == 0)
+        int max = Int32.MinValue;
+        for (int i = 0; i < nums.Length; i++) max = Math.Max(max, nums[i]);
+
+        int ans = 0;
+        while (k-- > 0)
         {
-            return nums[0];
-        }
-        else if (n == 1)
-        {
-            return Math.Max(nums[0], nums[1]);
+            ans += max;
+            max++;
         }
 
-        if (dp[n] != -1) return dp[n];
-
-        return dp[n] = Math.Max(nums[n] + Solve(nums, n - 2), Solve(nums, n - 1));
-    }
-    public static int Rob(int[] nums)
-    {
-        int n = nums.Length;
-
-        for (int i = 0; i < 401; i++)
-        {
-            dp[i] = -1;
-        }
-        return Solve(nums, n - 1);
+        return ans;
     }
 
     public static void Main(string[] args)
     {
-        int[] nums = { 1, 2, 3, 1 };
-        Console.WriteLine(Rob(nums));
+        int[] nums = { 2, 3, 4, 5 };
+        int k = 3;
+        Console.WriteLine(MaximizeSum(nums, k));
     }
 }
--- obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62801c589ee63d124e1bcef670bb731df48b52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
